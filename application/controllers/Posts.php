6d95834da43e7f15084db20675149c32ee76d5af
Mark Lehane <mklehane@gmail.com>
2017-01-15 20:15:41 +0000

    Work from Part 2 done

--- application/controllers/Posts.php
+++ application/controllers/Posts.php
@@ -29,4 +29,50 @@ class Posts extends CI_Controller
     $this->load->view('posts/view', $data);
     $this->load->view('templates/footer');
   }
+
+  public function create()
+  {
+    $data['title'] = 'Create Post';
+
+    $this->form_validation->set_rules('title', 'Title', 'required');
+    $this->form_validation->set_rules('body', 'Body', 'required');
+
+    if ($this->form_validation->run() === FALSE)
+    {
+      $this->load->view('templates/header');
+      $this->load->view('posts/create', $data);
+      $this->load->view('templates/footer');
+    } else
+    {
+      $this->Post_model->create_post();
+      redirect('posts');
+    }
+  }
+
+  public function delete($id)
+  {
+    $this->Post_model->delete_post($id);
+    redirect('posts');
+  }
+
+  public function edit($slug)
+  {
+    $data['post'] = $this->Post_model->get_posts($slug);
+
+    if (empty($data['post']))
+    {
+      show_404();
+    }
+    $data['title'] = 'Edit Post';
+
+    $this->load->view('templates/header');
+    $this->load->view('posts/edit', $data);
+    $this->load->view('templates/footer');
+  }
+
+  public function update()
+  {
+    $this->Post_model->update_post();
+    redirect('posts');
+  }
 }
