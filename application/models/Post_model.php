528394fa11ff43c28e7de7d10b3bd20c199d9f1b
Mark Lehane <mklehane@gmail.com>
2017-03-05 20:43:40 +0000

    Work from Part 4 done

--- application/models/Post_model.php
+++ application/models/Post_model.php
@@ -2,64 +2,73 @@
 
 class Post_model extends CI_Model
 {
-  public function __construct()
-  {
-    $this->load->database();
-  }
-
-  public function get_posts($slug = FALSE)
-  {
-    if ($slug === FALSE)
+    public function __construct()
     {
-      $this->db->order_by('posts.id', 'DESC');
-      $this->db->join('categories', 'categories.id = posts.category_id');
-      $query = $this->db->get('posts');
-      return $query->result_array();
+        $this->load->database();
     }
 
-    $query = $this->db->get_where('posts', array('slug' => $slug));
-    return $query->row_array();
-  }
+    public function get_posts($slug = false)
+    {
+        if ($slug === false) {
+            $this->db->order_by('posts.id', 'DESC');
+            $this->db->join('categories', 'categories.id = posts.category_id');
+            $query = $this->db->get('posts');
+            return $query->result_array();
+        }
 
-  public function create_post($post_image)
-  {
-    $slug = url_title($this->input->post('title'));
+        $query = $this->db->get_where('posts', array('slug' => $slug));
+        return $query->row_array();
+    }
 
-    $data = array(
-      'title' => $this->input->post('title'),
-      'slug' => $slug,
-      'body' => $this->input->post('body'),
-      'category_id' => $this->input->post('category_id'),
-      'post_image' => $post_image
-    );
-    return $this->db->insert('posts', $data);
-  }
+    public function create_post($post_image)
+    {
+        $slug = url_title($this->input->post('title'));
 
-  public function delete_post($id)
-  {
-    $this->db->where('id', $id);
-    $this->db->delete('posts');
-    return true;
-  }
+        $data = array(
+            'title'       => $this->input->post('title'),
+            'slug'        => $slug,
+            'body'        => $this->input->post('body'),
+            'category_id' => $this->input->post('category_id'),
+            'post_image'  => $post_image,
+        );
+        return $this->db->insert('posts', $data);
+    }
 
-  public function update_post()
-  {
-    $slug = url_title($this->input->post('title'));
+    public function delete_post($id)
+    {
+        $this->db->where('id', $id);
+        $this->db->delete('posts');
+        return true;
+    }
 
-    $data = array(
-      'title' => $this->input->post('title'),
-      'slug' => $slug,
-      'body' => $this->input->post('body'),
-      'category_id' => $this->input->post('category_id')
-    );
-    $this->db->where('id', $this->input->post('id'));
-    return $this->db->update('posts', $data);
-  }
+    public function update_post()
+    {
+        $slug = url_title($this->input->post('title'));
 
-  public function get_categories()
-  {
-    $this->db->order_by('name');
-    $query = $this->db->get('categories');
-    return $query->result_array();
-  }
+        $data = array(
+            'title'       => $this->input->post('title'),
+            'slug'        => $slug,
+            'body'        => $this->input->post('body'),
+            'category_id' => $this->input->post('category_id'),
+        );
+        $this->db->where('id', $this->input->post('id'));
+        return $this->db->update('posts', $data);
+    }
+
+    public function get_categories()
+    {
+        $this->db->order_by('name');
+        $query = $this->db->get('categories');
+        return $query->result_array();
+    }
+
+    public function get_posts_by_category($category_id)
+    {
+        $this->db->order_by('posts.id', 'DESC');
+        $this->db->join('categories', 'categories.id = posts.category_id');
+
+        $query = $this->db->get_where('posts', array('category_id' => $category_id));
+
+        return $query->result_array();
+    }
 }
