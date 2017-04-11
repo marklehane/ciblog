9e8e7dbbaa2eb51777bf548043f59d2e6cbabca6
Mark Lehane <mklehane@gmail.com>
2017-04-11 19:45:17 +0100

    Work on Part 6 done

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -9,25 +9,49 @@ class Users extends CI_Controller
         $data['title'] = 'Sign Up';
 
         $this->form_validation->set_rules('name', 'Name', 'required');
-        $this->form_validation->set_rules('username', 'Username', 'required');
-        $this->form_validation->set_rules('email', 'Email', 'required');
+        $this->form_validation->set_rules('username', 'Username', 'required|callback_check_username_exists');
+        $this->form_validation->set_rules('email', 'Email', 'required|callback_check_email_exists');
         $this->form_validation->set_rules('password', 'Password', 'required');
         $this->form_validation->set_rules('password2', 'Confirm Password', 'matches[password]');
 
-        if ($this->form_validation->run() == FALSE) {
-        	$this->load->view('templates/header');
-        	$this->load->view('users/register', $data);
-        	$this->load->view('templates/footer');
+        if ($this->form_validation->run() == false) {
+            $this->load->view('templates/header');
+            $this->load->view('users/register', $data);
+            $this->load->view('templates/footer');
         } else {
-        	//Encrypt Password
-        	$enc_password = md5($this->input->post('password'));
+            //Encrypt Password
+            $enc_password = md5($this->input->post('password'));
 
-        	$this->user_model->register($enc_password);
+            $this->user_model->register($enc_password);
 
-        	//Set Message
-        	$this->session->set_flashdata('user_registered', 'You are now registered and can login');
+            //Set Message
+            $this->session->set_flashdata('user_registered', 'You are now registered and can login');
 
-        	redirect('posts');
+            redirect('posts');
+        }
+    }
+
+    //Check if username exists
+    public function check_username_exists($username)
+    {
+        $this->form_validation->set_message('check_username_exists', 'That username is taken, please choose another.');
+
+        if ($this->user_model->check_username_exists($username)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //Check if email exists
+    public function check_email_exists($email)
+    {
+        $this->form_validation->set_message('check_email_exists', 'That email is not available');
+
+        if ($this->user_model->check_email_exists($email)) {
+            return true;
+        } else {
+            return false;
         }
     }
 
--- application/models/User_model.php
+++ application/models/User_model.php
@@ -1,23 +1,44 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class User_model extends CI_Model {
+class User_model extends CI_Model
+{
 
-	public function register($enc_password)
-	{
-		//User data array
-		$data = array(
-			'name' => $this->input->post('name'),
-			'email' => $this->input->post('email'),
-			'username' => $this->input->post('username'),
-			'zipcode' => $this->input->post('zipcode'),
-			'password' => $enc_password,
-			'zipcode' => $this->input->post('zipcode'),
-		);
+    public function register($enc_password)
+    {
+        //User data array
+        $data = array(
+            'name'     => $this->input->post('name'),
+            'email'    => $this->input->post('email'),
+            'username' => $this->input->post('username'),
+            'zipcode'  => $this->input->post('zipcode'),
+            'password' => $enc_password,
+            'zipcode'  => $this->input->post('zipcode'),
+        );
 
-		//Insert User
-		return $this->db->insert('users', $data);
-	}
+        //Insert User
+        return $this->db->insert('users', $data);
+    }
+
+    public function check_username_exists($username)
+    {
+        $query = $this->db->get_where('users', array('username' => $username));
+        if (empty($query->row_array())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function check_email_exists($email)
+    {
+        $query = $this->db->get_where('users', array('email' => $email));
+        if (empty($query->row_array())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 }
 
